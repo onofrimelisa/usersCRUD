f31d6147e61af829b371c5571d9996c46480f3b3
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-04-28 10:21:48 -0300

    feat: add the rest of the endpoints - PUT and DELETE

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func main() {
 		app.GET("/",  GetUsers)
 		app.GET("/:id",  GetUser)
 		app.POST("/",  CreateUser)
+		app.PUT("/:id",  UpdateUser)
+		app.DELETE("/:id",  DeleteUser)
 	}
 
 	err := r.Run(":8080")
@@ -29,12 +31,38 @@ func main() {
 	}
 }
 
+func UpdateUser(c *gin.Context) {
+	db := InitDb()
+	defer db.Close()
+
+	id := c.Params.ByName("id")
+	var user User
+	db.First(&user, id)
+
+	if user.Id != 0 {
+		var newUser User
+		c.Bind(&newUser)
+
+		if !isEmpty(c, newUser.Firstname, newUser.Lastname) {
+			result := User{
+				Id: user.Id,
+				Firstname: newUser.Firstname,
+				Lastname:  newUser.Lastname,
+			}
+
+			db.Save(&result)
+			c.JSON(200, gin.H{"success": result})
+		}
+	} else {
+		c.JSON(404, gin.H{"error": "User not found"})
+	}
+}
+
 func GetUser(c *gin.Context) {
 	db := InitDb()
 	defer db.Close()
 
 	id := c.Params.ByName("id")
-
 	var user User
 	db.First(&user, id)
 
@@ -62,12 +90,37 @@ func CreateUser(c *gin.Context) {
 	var user User
 	c.Bind(&user)
 
-	if user.Firstname != "" && user.Lastname != "" {
+	if !isEmpty(c, user.Firstname, user.Lastname) {
 		db.Create(&user)
 		c.JSON(201, gin.H{"success": user})
-	} else {
-		c.JSON(422, gin.H{"error": "Fields are empty"})
 	}
+}
+
+func DeleteUser(c *gin.Context) {
+	db := InitDb()
+	defer db.Close()
+
+	id := c.Params.ByName("id")
+	var user User
+	db.First(&user, id)
+
+	if user.Id != 0 {
+		db.Delete(&user)
+		c.JSON(200, gin.H{"success": "User #" + id + " deleted"})
+	} else {
+		c.JSON(404, gin.H{"error": "User not found"})
+	}
+}
+
+func isEmpty(c *gin.Context, fields ...string) bool {
+	for _, field := range fields {
+		if field == "" {
+			c.JSON(422, gin.H{"error": "Fields are empty"})
+			return true
+		}
+	}
+
+	return false
 }
 
 func InitDb() *gorm.DB {
